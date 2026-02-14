d73469fb0a1d965244d0fc40bbdb833f2e881eba
Tryfon Papadopoulos <50261221+trifpap@users.noreply.github.com>
2026-02-15 00:20:22 +0200

    Update 2 for app.py

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
     return "API is running!"
 
-@app.route("/process", methods=["GET", "POST"])
+@app.route("/process", methods=["POST"])
 def process_text():
-    if request.method == "POST":
-        data = request.json
-        text = data.get("text", "")
-    else:
-        text = request.args.get("text", "")
-    
+    data = request.json
+    text = data.get("text", "")
     return jsonify({"result": text.upper()})
